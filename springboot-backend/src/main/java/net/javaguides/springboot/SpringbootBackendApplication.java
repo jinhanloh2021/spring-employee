da42f8067faddf2c36e642e88acfe32392cdf5c1
unknown <jinhan.loh.2021@scis.smu.edu.sg>
2022-09-13 21:04:43 +0800

    Finish backend.

--- springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -1,34 +1,35 @@
 package net.javaguides.springboot;
 
-// import net.javaguides.springboot.model.Employee;
-// import net.javaguides.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
 
-	// @Autowired
-	// private EmployeeRepository employeeRepository;
+	@Autowired
+	private EmployeeRepository employeeRepository;
 
-	// @Override
-	// public void run(String... args) throws Exception {
-	// // Employee employee = new Employee();
-	// // employee.setFirstName("Ramesh");
-	// // employee.setLastName("Fadatare");
-	// // employee.setEmailId("ramesh@gmail.com");
-	// // employeeRepository.save(employee);
-	// //
-	// // Employee employee1 = new Employee();
-	// // employee1.setFirstName("John");
-	// // employee1.setLastName("Cena");
-	// // employee1.setEmailId("cena@gmail.com");
-	// // employeeRepository.save(employee1);
-	// }
+	@Override
+	public void run(String... args) throws Exception {
+		Employee employee = new Employee();
+		employee.setFirstName("Jin Han");
+		employee.setLastName("Loh");
+		employee.setEmailId("test@test.com");
+		employeeRepository.save(employee);
+
+		Employee employee2 = new Employee();
+		employee2.setFirstName("Jinathan");
+		employee2.setLastName("Han");
+		employee2.setEmailId("jinathan@test.com");
+		employeeRepository.save(employee2);
+	}
 }
--- springboot-backend/src/main/java/net/javaguides/springboot/model/Employee.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/model/Employee.java
@@ -1,5 +1,29 @@
 package net.javaguides.springboot.model;
 
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Employees")
 public class Employee {
-  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(name = "first_name", nullable = false)
+  private String firstName;
+
+  @Column(name = "last_name", nullable = true)
+  private String lastName;
+
+  @Column(name = "email_id", nullable = false)
+  private String emailId; // if do not specify, columnName will be "emailId"
 }
